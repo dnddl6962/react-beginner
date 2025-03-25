9a848d0dfed24745bf4830912e8389cc01ecd966
dnddl6962 <77288155+dnddl6962@users.noreply.github.com>
2025-03-25 20:42:50 +0900

    use cleanup function

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -2,30 +2,21 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
-function App() {
-
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (e) => setKeyword(e.target.value);
-  console.log('I run all the time');
-  useEffect(() => console.log("I run only once"), [])
+function Hello() {
   useEffect(() => {
-    if (keyword !== "" && keyword.length > 5) {
-      console.log("Search for", keyword)
-    }
-  }
-    , [keyword]);
+    console.log("created :(");
+    return () => console.log("Destroyed :)"); //clean up function
+  }, [])
+  return <h1>Hello</h1>;
+}
+
+function App() {
+  const [value, setValue] = useState(false);
+  const onClick = () => setValue((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search Here.."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {value ? <Hello /> : null}
+      <button onClick={onClick}>{value ? "Hide" : "Show"}</button>
     </div>
   );
 }

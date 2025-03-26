09c301afe4e4cd04cbdcbec1c7eceb3042962fe1
dnddl6962 <77288155+dnddl6962@users.noreply.github.com>
2025-03-26 14:58:00 +0900

    basic to do list with react

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -2,21 +2,30 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
-function Hello() {
-  useEffect(() => {
-    console.log("created :(");
-    return () => console.log("Destroyed :)"); //clean up function
-  }, [])
-  return <h1>Hello</h1>;
-}
 
 function App() {
-  const [value, setValue] = useState(false);
-  const onClick = () => setValue((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (e) => setToDo(e.target.value);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  }
   return (
     <div>
-      {value ? <Hello /> : null}
-      <button onClick={onClick}>{value ? "Hide" : "Show"}</button>
+      <h1>My To Dos {toDos.length}</h1>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do" />
+        <button>Add To DO</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => <li key={index}>{item}</li>)}
+      </ul>
     </div>
   );
 }
